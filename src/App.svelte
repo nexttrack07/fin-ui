4a3aef002c3d105ed0d6db0eedaac5cbac488c76
Faadhil Hakkim <faadhil1991@gmail.com>
2022-05-03 22:04:35 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Router } from "@sveltech/routify";
+	import { routes } from "@sveltech/routify/tmp/routes";
+</script>
+
+<style global>
+	@tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<Router {routes} />
